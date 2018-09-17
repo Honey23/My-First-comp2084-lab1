17a136b03b7ec60157a8fef9ca6c34f0dfe59f60
200374847 <200374847@student.georgianc.on.ca>
2018-09-17 06:33:04 -0700

    Completing Lab1

---
+++ Lab1_Honey/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab1_Honey.Startup))]
+namespace Lab1_Honey
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
